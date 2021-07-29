c432d36984aa6c4d21a591387df738750f972317
KrivovDima <krivovdima89@gmail.com>
2021-07-29 19:37:51 +0400

    refactor state, add store

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import state, {subscribe} from "./redux/state";
+import store, {stateType} from "./redux/state";
 import ReactDOM from "react-dom";
 import App from "./App";
-import {addPost, stateType, updateTextPost} from "./redux/state";
 
 function rerenderAllTree(state: stateType) {
-  ReactDOM.render(<App state = {state} addPost = {addPost} updateTextPost={updateTextPost}/>,
+  ReactDOM.render(<App state={store.getState()}
+                       addPost={store.addPost.bind(store)}
+                       updateTextPost={store.updateTextPost.bind(store)}/>,
     document.getElementById('root'));
 }
 
-rerenderAllTree(state);
+rerenderAllTree(store.getState());
 
-subscribe(rerenderAllTree);
+store.subscribe(rerenderAllTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.ts
+++ src/redux/state.ts
@@ -27,55 +27,65 @@ export type stateType = {
   dialogsPage: dialogsPageType
 }
 
-let rerenderAllTree = (state: stateType) => {
-
-};
-
-const state: stateType = {
-  profilePage: {
-    postsData: [
-      {id: 1, message: 'Lorem ipsum dolor sit amet', likeCount: 11},
-      {id: 2, message: 'Hello, how are you', likeCount: 20},
-    ],
-    newTextPost: '',
-  },
-  dialogsPage: {
-    dialogsData: [
-      {id: 1, name: 'Никита'},
-      {id: 2, name: 'Стас'},
-      {id: 3, name: 'Гена'},
-      {id: 4, name: 'Турбо'},
-      {id: 5, name: 'Дюша Метелкин'},
-    ],
-    messagesData: [
-      {id: 1, message: 'Lorem ipsum dolor sit amet, consectetur adipisicing'},
-      {id: 2, message: 'Lorem ipsum dolor sit amet'},
-      {id: 3, message: 'Lorem ipsum dolor sit amet, consectetur'},
-    ],
-  }
-};
-
 export type addPostType = () => void
 
-export const addPost = () => {
-  const newPost = {
-    id: 5,
-    message: state.profilePage.newTextPost,
-    likeCount: 0
-  };
-  state.profilePage.postsData.push(newPost);
-  state.profilePage.newTextPost = '';
-  rerenderAllTree(state);
+type observerType = (state: stateType) => void
+
+export type storeType = {
+  _state: stateType
+  _rerenderAllTree: (state: stateType) => void
+  addPost: () => void
+  updateTextPost: (text: string) => void
+  getState: () => stateType
+  subscribe: (observer: observerType) => void
 }
 
-export const updateTextPost = (text: string) => {
-  state.profilePage.newTextPost = text;
-  rerenderAllTree(state);
+const store: storeType = {
+  _state: {
+    profilePage: {
+      postsData: [
+        {id: 1, message: 'Lorem ipsum dolor sit amet', likeCount: 11},
+        {id: 2, message: 'Hello, how are you', likeCount: 20},
+      ],
+      newTextPost: '',
+    },
+    dialogsPage: {
+      dialogsData: [
+        {id: 1, name: 'Никита'},
+        {id: 2, name: 'Стас'},
+        {id: 3, name: 'Гена'},
+        {id: 4, name: 'Турбо'},
+        {id: 5, name: 'Дюша Метелкин'},
+      ],
+      messagesData: [
+        {id: 1, message: 'Lorem ipsum dolor sit amet, consectetur adipisicing'},
+        {id: 2, message: 'Lorem ipsum dolor sit amet'},
+        {id: 3, message: 'Lorem ipsum dolor sit amet, consectetur'},
+      ],
+    }
+  },
+  getState() {
+    return this._state;
+  },
+  _rerenderAllTree(state: stateType) {
+  },
+  addPost() {
+    const newPost = {
+      id: 5,
+      message: this._state.profilePage.newTextPost,
+      likeCount: 0
+    };
+    this._state.profilePage.postsData.push(newPost);
+    this._state.profilePage.newTextPost = '';
+    this._rerenderAllTree(this._state);
+  },
+  updateTextPost(text: string) {
+    this._state.profilePage.newTextPost = text;
+    this._rerenderAllTree(this._state);
+  },
+  subscribe(observer: observerType) {
+    this._rerenderAllTree = observer;
+  }
 }
 
- export const subscribe = (observer: (state: stateType) => void) => {
-  rerenderAllTree = observer;
-}
-
-
-export default state;
+export default store;
